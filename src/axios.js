506c9e03be5201823e1249b1f8bbfa965c6e6c72
Indran  Jayatharan <indran@calcey.com>
2021-11-30 19:05:02 +0530

    baseUrl changed

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //axios.defaults.baseURL = 'http://192.168.1.100:5000';
 //axios.defaults.baseURL = 'http://localhost:5000';
-axios.defaults.baseURL = 'http://165.22.217.143:5000';
+axios.defaults.baseURL = 'https://test.nammakadai.lk';
 //axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 //axios.defaults.headers.post['Content-Type'] = 'application/json';
 
